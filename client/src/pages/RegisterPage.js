5ea933a4fcf849d250fdfa6d08e66403cd2e0a4e
priyanair9 <pnair9090@gmail.com>
2024-02-05 15:55:21 +1100

    Made register form

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -3,7 +3,12 @@ import React from 'react';
 function RegisterPage(props) {
     return (
         <div>
-            
+            <form>
+                <input type="email" name="email" size="30" placeholder="Email"></input>
+                <input name="username" size="30" placeholder="Username"></input>
+                <input type="password" name="password" size="30" placeholder="Password"></input>
+                <input type="password" name="confirm-password" size="30" placeholder="Confirm password"></input>
+            </form>
         </div>
     );
 }
