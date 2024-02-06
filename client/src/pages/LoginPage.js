6de292eda200e787d9c56f26476ca3f1e711f19e
priyanair9 <pnair9090@gmail.com>
2024-02-07 10:54:09 +1100

    Created login form ui

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -3,7 +3,12 @@ import React from 'react';
 function LoginPage(props) {
     return (
         <div>
-            
+            <h1>Login</h1>
+            <form className='register-form'>
+                <input name="username" size="30" placeholder="Username"></input>
+                <input type="password" name="password" size="30" placeholder="Password"></input>
+                <button>Login</button>
+            </form>
         </div>
     );
 }
