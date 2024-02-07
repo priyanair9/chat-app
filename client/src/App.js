31eff3aaf218d919337dc5b2dd95a9cbd934894a
priyanair9 <pnair9090@gmail.com>
2024-02-07 16:05:18 +1100

    Fixed css

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 
 import RegisterPage from './pages/RegisterPage';
-import ChatsPage from './pages/ChatsPage';
 
 function App() {
   return (
     <div className="App">
-      <ChatsPage/>
+      <RegisterPage/>
     </div>
   );
 }
