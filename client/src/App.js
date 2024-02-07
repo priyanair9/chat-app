a5484bcaa57f82cf7847d05f86f726e620e6b1e6
priyanair9 <pnair9090@gmail.com>
2024-02-07 11:46:04 +1100

    Created chat screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 
 import RegisterPage from './pages/RegisterPage';
+import ChatsPage from './pages/ChatsPage';
 
 function App() {
   return (
     <div className="App">
-      <RegisterPage/>
+      <ChatsPage/>
     </div>
   );
 }
--- client/src/pages/ChatsPage.js
+++ client/src/pages/ChatsPage.js
@@ -2,9 +2,34 @@ import React from 'react';
 
 function ChatsPage(props) {
     return (
-        <div>
-            
+        <div class="chat-container">
+        {/* Sidebar for chats */}
+        <div class="sidebar">
+          <div class="search-bar">
+            <input type="text" placeholder="Search chats"></input>
+          </div>
+          <div class="chat-list">
+            {/*  List of chats */}
+            {/* <!-- Each chat item should contain profile picture, name, last message, etc. --> */}
+          </div>
         </div>
+        {/* <!-- Main chat area --> */}
+        <div class="chat-area">
+          {/* <!-- Chat header with user details --> */}
+          <div class="chat-header">
+            {/* <!-- Display user's profile picture, name, status, etc. --> */}
+          </div>
+          {/* <!-- Chat messages --> */}
+          <div class="messages">
+            {/* <!-- Individual message bubbles --> */}
+          </div>
+          {/* <!-- Message input area --> */}
+          <div class="message-input">
+            <textarea placeholder="Type your message..."></textarea>
+            <button>Send</button>
+          </div>
+        </div>
+      </div>
     );
 }
 
