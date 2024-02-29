9761ad4361a8a6ad8b4bc27055755e14168bfca2
priyanair9 <pnair9090@gmail.com>
2024-02-29 14:29:45 +1100

    Created routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
-
+import {Routes, Route, Navigate } from "react-router-dom";
 import RegisterPage from './pages/RegisterPage';
-// import ChatsPage from './pages/ChatsPage';
+import LoginPage from './pages/LoginPage';
+import ChatsPage from './pages/ChatsPage';
 // import ChatCard from './components/ChatCard';
 
 function App() {
   return (
-    <div className="App">
-      <RegisterPage/>
+    <div className='App'>
+      <Routes>
+          <Route path='/' element= {<LoginPage/>}/>
+          <Route path='/login' element= {<LoginPage/>}/>
+          <Route path='/register' element={<RegisterPage/>}/>
+          <Route path='/chats' element={<ChatsPage/>}/>
+      </Routes>
     </div>
   );
 }
