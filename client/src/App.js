cbd1041f7ffc6486abcd6a667a9f9c90ba9eec48
priyanair9 <pnair9090@gmail.com>
2024-02-29 16:07:04 +1100

    Removed unused import

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Routes, Route, Navigate } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import ChatsPage from './pages/ChatsPage';
