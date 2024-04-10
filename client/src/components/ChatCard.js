574b149f059ff39c57108722395c8f140952b664
priyanair9 <pnair9090@gmail.com>
2024-04-10 23:20:25 +1000

    Fixed chat card component css and alignment

--- client/src/components/ChatCard.js
+++ client/src/components/ChatCard.js
@@ -3,10 +3,12 @@ import React from 'react';
 function ChatCard(props) {
     return (
         <div className='card-container'>
-            <p className='username'>random.user</p>
-            <p className='last-message'>Sent an attatchment</p>
             <div className='profile-pic'>
                 <p className='abreviation'>R</p>
+            </div>
+            <div className='card-content'>
+                <p className='username'>random.user</p>
+                <p className='last-message'>Sent an attatchment</p>
             </div>
         </div>
     );
