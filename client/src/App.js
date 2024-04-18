df78de652d7d03590f835e838116083347141411
priyanair9 <pnair9090@gmail.com>
2024-04-18 19:27:52 +1000

    Created home page and fixed header css

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import {Routes, Route} from "react-router-dom";
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import ChatsPage from './pages/ChatsPage';
-// import ChatCard from './components/ChatCard';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className='App'>
       <Routes>
-          <Route path='/' element= {<LoginPage/>}/>
+          <Route path='/' element= {<HomePage/>}/>
           <Route path='/login' element= {<LoginPage/>}/>
           <Route path='/register' element={<RegisterPage/>}/>
           <Route path='/chats' element={<ChatsPage/>}/>
---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage(props) {
+    return (
+        <div>
+            <p>Home page</p>
+        </div>
+    );
+}
+
+export default HomePage;
