1d695cf2f1d07e39a6d950bd7cef61004791264f
priyanair9 <pnair9090@gmail.com>
2024-04-18 22:36:29 +1000

    Added and styled buttons

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function HomePage(props) {
     return (
         <div>
-            <p>Home page</p>
+            <div className='homepage-content'>
+                <p>ChatApp</p>
+                <Link to= {"/login"}className='login'>Login</Link>
+                <Link to= {"/register"} className='register'>Register</Link>
+            </div>
+            <div className='artwork'></div>
         </div>
     );
 }
